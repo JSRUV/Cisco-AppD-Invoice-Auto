e2abce6f021e0a9ed2cafca34c30ba260904ade4
JSRUV <jruvalcaba@mail.sfsu.edu>
2023-11-01 21:41:46 -0700

    added files + dataframe

--- invoice.py
+++ invoice.py
@@ -4,3 +4,17 @@ from docxtpl import DocxTemplate
 from babel.numbers import format_currency
 from docx2pdf import convert
 
+base_dir = Path(__file__).parent
+invoiceTemplate = input("Enter the template name ")
+invoiceTemplatePath = base_dir/invoiceTemplate
+
+excelPath = base_dir / "dummy_data.xlsx"
+
+output_dir = base_dir / "invoices"
+output_dir.mkdir(exist_ok=True)
+
+invoice = pd.read_excel(excelPath, sheet_name="Sheet1")
+
+##################### Invoice Start #######################
+invoice = pd.DataFrame(invoice)
+print(invoice)
